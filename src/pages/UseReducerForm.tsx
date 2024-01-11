4bc103f5f20e7443ebfc4069c3d08d9ac6a20078
masudmolla6 <masudmolla2937@gmail.com>
2024-01-11 20:34:24 +0600

    complete Reducer form

--- src/pages/UseReducerForm.tsx
+++ src/pages/UseReducerForm.tsx
@@ -1,8 +1,13 @@
-import React, { useReducer } from 'react';
+import React, { ChangeEvent, useReducer } from 'react';
+
+type TAction = {
+  type: string;
+  payload: string;
+}
 
 const initialState = { name:"", email:"" };
 
-const reducer = (currentState, action) => {
+const reducer = (currentState:typeof initialState, action:TAction) => {
     switch (action.type) {
 
         case 'addName':
@@ -18,12 +23,17 @@ const reducer = (currentState, action) => {
 
 const UseReducerForm = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSubmit = (event:ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+      console.log(state);
+    }
     return (
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left"></div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body w-96">
+            <form onSubmit={handleSubmit} className="card-body w-96">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
@@ -31,6 +41,7 @@ const UseReducerForm = () => {
                 <input
                   type="text"
                   name="name"
+                  onChange={(event)=>dispatch({type:"addName", payload:event.target.value})}
                   placeholder="Enter Your Name"
                   className="input input-bordered"
                   required
@@ -43,6 +54,7 @@ const UseReducerForm = () => {
                 <input
                   type="Email"
                   name="email"
+                  onChange={(event)=>dispatch({type:"addEmail", payload:event.target.value})}
                   placeholder="Enter Your Email"
                   className="input input-bordered"
                   required
